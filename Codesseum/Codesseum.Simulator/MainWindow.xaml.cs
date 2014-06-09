4f14a3b50c4bd6388116aef45c079509337e90d7
Istvan Reiter <reiteristvan@users.noreply.github.com>
2014-06-09 09:19:55 +0000

    Added Simulator project

---
+++ Codesseum/Codesseum.Simulator/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Codesseum.Simulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Codesseum/Codesseum.Simulator/Models/BotInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Codesseum.Simulator.Models
+{
+    public class BotInfo
+    {
+        public string TeamName { get; set; }
+        public string Path { get; set; }
+    }
+}
