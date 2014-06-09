e8b7edbfedf3fa179e215041c77c9a00efe3fc8f
Istvan Reiter <reiteristvan@users.noreply.github.com>
2014-06-09 11:47:47 +0000

    Added SimulatorWindow and Configuration started

---
+++ Codesseum/Codesseum.Simulator/SimulatorWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Codesseum.Simulator.Messages;
+using Codesseum.Simulator.Models;
+using Codesseum.Simulator.ViewModels;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace Codesseum.Simulator
+{
+    /// <summary>
+    /// Interaction logic for SimulatorWindow.xaml
+    /// </summary>
+    public partial class SimulatorWindow
+    {
+        public SimulatorWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Show(IEnumerable<BotInfoModel> bots)
+        {
+            DataContext = new SimulatorViewModel(bots);
+            Show();
+
+            Messenger.Default.Send(new StartMessage());
+        }
+    }
+}
