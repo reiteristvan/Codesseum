cb95289bfd0adb9a065efb4109011e568df89c73
Istvan Reiter <reiteristvan@users.noreply.github.com>
2014-06-22 10:31:20 +0000

    Added Pair type, Points now ObservableCollection

---
+++ Codesseum/Codesseum.Common/Types/Pair.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+
+namespace Codesseum.Common.Types
+{
+    public class Pair : INotifyPropertyChanged
+    {
+        private string key = "Key";
+        public string Key
+        {
+            get { return key; }
+            set
+            {
+                if (key != value)
+                {
+                    key = value;
+                    NotifyPropertyChanged("Key");
+                }
+            }
+        }
+
+
+        private double value;
+        public double Value
+        {
+            get { return value; }
+            set
+            {
+                if (this.value != value)
+                {
+                    this.value = value;
+                    NotifyPropertyChanged("Value");
+                }
+            }
+        }
+
+        public Pair() { }
+        public Pair(string key, double value)
+            : this()
+        {
+            Key = key;
+            Value = value;
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
