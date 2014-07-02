fc482b81fcbb9fcd39bcadb8e68dc38a7fa95d99
Istvan Reiter <reiteristvan@users.noreply.github.com>
2014-07-02 13:57:50 +0000

    Added beta subscribe page and models to site

---
+++ Codesseum/Codesseum.Web/Areas/Beta/BetaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Codesseum.Web.Areas.Beta
+{
+    public class BetaAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Beta";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Beta_default",
+                "Beta/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Codesseum/Codesseum.Web/Areas/Beta/Controllers/SubscribeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Codesseum.Web.Areas.Beta.Models;
+
+namespace Codesseum.Web.Areas.Beta.Controllers
+{
+    public class SubscribeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Index(SubscribeModel model)
+        {
+            return Redirect("");
+        }
+    }
+}
