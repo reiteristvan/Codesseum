7567ffad3e57a35757967a1f81299344da473f20
Istvan Reiter <istvanreiter@outlook.com>
2014-07-02 13:57:50 +0000

    Added beta subscribe page and models to site

---
+++ Codesseum/Codesseum.Web/Areas/Beta/Models/SubscribeModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Codesseum.Web.Areas.Beta.Models
+{
+    public class SubscribeModel
+    {
+        [Display(Name = "Felhasználónév")]
+        public string Username { get; set; }
+
+        [Display(Name = "Email cím")]
+        public string Email { get; set; }
+    }
+}
