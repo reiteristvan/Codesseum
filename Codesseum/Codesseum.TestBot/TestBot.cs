f427d3bedd9b59a6bdce5b27edc28a1d41c80b17
Istvan Reiter <istvanreiter@outlook.com>
2014-07-03 07:37:03 +0000

    Modify TestBot for better testing

--- Codesseum/Codesseum.TestBot/TestBot.cs
+++ Codesseum/Codesseum.TestBot/TestBot.cs
@@ -12,30 +12,70 @@ namespace Codesseum.TestBot
             TeamName = "almabeka";
         }
 
+        private Coordinate GetDirection(Direction direction, Coordinate origin)
+        {
+            Coordinate result;
+
+            switch (direction)
+            {
+                case Direction.Down:
+                    result = new Coordinate(origin.X, origin.Y + 1);
+                    break;
+                case Direction.Left:
+                    result = new Coordinate(origin.X - 1, origin.Y);
+                    break;
+                case Direction.Right:
+                    result = new Coordinate(origin.X + 1, origin.Y);
+                    break;
+                case Direction.Top:
+                    result = new Coordinate(origin.X, origin.Y - 1);
+                    break;
+                default:
+                    return origin;
+            }
+
+            return result;
+        }
+
         public override BotAction NextAction()
         {
-            var random = new Random();
-            Coordinate target;
+            var result = new BotAction { Action = ActionType.Move };
+            bool success = false;
 
-            if (random.Next(1000)%2 == 0)
+            while (!success)
             {
-                target = new Coordinate(Position.X + 5, Position.Y);
-            }
-            else
-            {
-                target = new Coordinate(Position.X, Position.Y + 5);
+                Direction direction;
+                lock (_locker)
+                {
+                    direction = (Direction)random.Next(0, 4);
+                }
+
+                var target = GetDirection(direction, Position);
+
+                if (World.Current.Map[target] != -1)
+                {
+                    result.Target = target;
+                    success = true;
+                }
             }
 
-            return new BotAction
-            {
-                Action = ActionType.Move,
-                Target = target
-            };
+            return result;
         }
 
         public override int[] GetAttributes()
         {
             return new[] { 5, 5, 5, 5, 5 };
         }
+
+        private static readonly  object _locker = new object();
+        private static readonly Random random = new Random();
+    }
+
+    public enum Direction
+    {
+        Left = 0,
+        Right = 1,
+        Top = 2,
+        Down = 3
     }
 }
